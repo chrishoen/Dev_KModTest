4dd4f46007215c9ff84ccdf6dea4d68a4890a5cc
chrishoen <chris123hoen@gmail.com>
2023-04-16 19:22:16 -0400

    prints

--- CommonLib/cmnGPIO.cpp
+++ CommonLib/cmnGPIO.cpp
@@ -29,38 +29,38 @@ void GPIO::initialize()
    // Chip 0.
    if ((mChip0 = gpiod_chip_open_by_name("gpiochip0")) == 0)
    {
-      printf("GPIO::initialize FAIL1");
+      printf("GPIO::initialize FAIL1\n");
       return;
    }
 
    // Chip 1.
    if ((mChip1 = gpiod_chip_open_by_name("gpiochip1")) == 0)
    {
-      printf("GPIO::initialize FAIL2");
+      printf("GPIO::initialize FAIL2\n");
       return;
    }
 
    // LineA. Chip0 Line5. Gpio5. Pin29. Output.
    if ((mLineA = gpiod_chip_get_line(mChip0, 5)) == 0)
    {
-      printf("GPIO::initialize FAIL3");
+      printf("GPIO::initialize FAIL3\n");
       return;
    }
    if (gpiod_line_request_output(mLineA, "CmnGpio", 0))
    {
-      printf("GPIO::initialize FAIL4");
+      printf("GPIO::initialize FAIL4\n");
       return;
    }
 
    // LineB. Chip0 Line6. Gpio6. Pin31. Input.
    if ((mLineB = gpiod_chip_get_line(mChip0, 6)) == 0)
    {
-      printf("GPIO::initialize FAIL4");
+      printf("GPIO::initialize FAIL4\n");
       return;
    }
    if (gpiod_line_request_input(mLineB, "CmnGpio"))
    {
-      printf("GPIO::initialize FAIL4");
+      printf("GPIO::initialize FAIL4\n");
       return;
    }
 }
@@ -80,7 +80,7 @@ void GPIO::writeA(bool aValue)
 {
    if (gpiod_line_set_value(mLineA, aValue))
    {
-      printf("GPIO::WriteA");
+      printf("GPIO::WriteA FAIL\n");
       return;
    }
 }
